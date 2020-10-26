e124cee3abc93a78025af7136da4ac8253acf5b9
Eduardo Andrade <eduardo.a@edu.pucrs.br>
2020-10-26 18:02:30 -0300

    Create Instruction.cs

---
+++ TP2/Instruction.cs
@@ -0,0 +1,14 @@
+﻿namespace TP2
+{
+    public struct Instruction
+    {
+        public int opcode;
+        public int funct;
+
+        public Instruction(int opcode, int funct)
+        {
+            this.opcode = opcode;
+            this.funct = funct;
+        }
+    }
+}
