b0380a999cdab908e1ac58b200abea4fc0035255
Eduardo Andrade <eduardo.a@edu.pucrs.br>
2020-10-26 18:02:37 -0300

    Create Program.cs

---
+++ TP2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace TP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Any() && File.Exists(args[0]))
+            {
+                Assembler asm = new Assembler(File.OpenText(args[0]));
+
+                return;
+            }
+
+            Console.WriteLine("Informe o caminho do arquivo.");
+        }
+    }
+}
