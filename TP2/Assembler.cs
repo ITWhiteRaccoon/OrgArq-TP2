478355020b4638664a20533b8e041b2585916ea0
Eduardo Andrade <eduardo.a@edu.pucrs.br>
2020-10-28 14:47:53 -0300

    jal ready

--- TP2/Assembler.cs
+++ TP2/Assembler.cs
@@ -1,71 +1,149 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace TP2
 {
     public class Assembler
     {
-        private StreamReader _input;
-        private Dictionary<string, Instruction> _nameToOp;
-        private Dictionary<string, int> _regToNum;
+        private const int StartAddress = 0x00400000;
 
-        public Assembler(StreamReader input)
+        private static Dictionary<string, Instruction> _nameToOp = new Dictionary<string, Instruction>
         {
-            _input = input;
+            {"sll", new Instruction(0, 0)},
+            {"div", new Instruction(0, 0x1a)},
+            {"nor", new Instruction(0, 0x27)},
 
-            _nameToOp = new Dictionary<string, Instruction>
+            {"jr", new Instruction(0, 8)},
+
+            {"beq", new Instruction(0x04, 0)},
+            {"blez", new Instruction(0x06, 0)},
+            {"slti", new Instruction(0x0a, 0)},
+            {"ori", new Instruction(0x0d, 0)},
+            {"lw", new Instruction(0x23, 0)},
+
+            {"jal", new Instruction(0x03, 0)},
+        };
+
+        private static Dictionary<string, int> _regToNum = new Dictionary<string, int>
+        {
+            {"$zero", 0},
+            {"$at", 1},
+            {"$v0", 2},
+            {"$v1", 3},
+            {"$a0", 4},
+            {"$a1", 5},
+            {"$a2", 6},
+            {"$a3", 7},
+            {"$t0", 8},
+            {"$t1", 9},
+            {"$t2", 10},
+            {"$t3", 11},
+            {"$t4", 12},
+            {"$t5", 13},
+            {"$t6", 14},
+            {"$t7", 15},
+            {"$s0", 16},
+            {"$s1", 17},
+            {"$s2", 18},
+            {"$s3", 19},
+            {"$s4", 20},
+            {"$s5", 21},
+            {"$s6", 22},
+            {"$s7", 23},
+            {"$t8", 24},
+            {"$t9", 25},
+            {"$k0", 26},
+            {"$k1", 27},
+            {"$gp", 28},
+            {"$sp", 29},
+            {"$fp", 30},
+            {"$ra", 31},
+        };
+
+        public static IEnumerable<string> Assemble(string[] input)
+        {
+            var result = new List<string>();
+            var labels = new Dictionary<string, int>();
+
+            SearchLabels(input, labels); //Procura por labels primeiro para poder completar em caso jal label
+
+            input = input.Where(x => !string.IsNullOrWhiteSpace(x) && !x.Equals(".text")).ToArray();
+
+            int currentLine = 0;
+            foreach (string line in input)
             {
-                {"sll", new Instruction(0, 0)},
-                {"div", new Instruction(0, 0x1a)},
-                {"nor", new Instruction(0, 0x27)},
+                string bin = "";
 
-                {"jr", new Instruction(0, 8)},
+                string[] content = line.Split(' ', ',');
+                content = content.Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
 
-                {"beq", new Instruction(0x04, 0)},
-                {"blez", new Instruction(0x06, 0)},
-                {"slti", new Instruction(0x0a, 0)},
-                {"ori", new Instruction(0x0d, 0)},
-                {"lw", new Instruction(0x23, 0)},
+                string instr = content[0];
+                if (_nameToOp.ContainsKey(instr))
+                {
+                    bin = Convert.ToString(_nameToOp[instr].opcode, 2)[^6..];
 
-                {"jal", new Instruction(0x03, 0)},
-            };
+                    switch (instr)
+                    {
+                        case "jal":
+                            bin += Convert.ToString(labels[content[1]], 2).PadLeft(32, '0')[4..^2];
+                            break;
+                        case "jr":
+                            break;
+                        case "sll":
+                        case "div":
+                        case "nor":
+                            break;
+                        case "slti":
+                        case "lw":
+                        case "beq":
+                        case "blez":
+                        case "ori":
+                            break;
+                    }
+                    result.Add(Convert.ToInt32(bin,2).ToString("x8"));
+                }
+            }
 
-            _regToNum = new Dictionary<string, int>
+            return result;
+        }
+
+        private static void SearchLabels(string[] input, Dictionary<string, int> labels)
+        {
+            int currentLine = 0;
+
+            bool openLabel = false;
+            string lastOpenLabel = "";
+            for (int i = 0; i < input.Length; i++)
             {
-                {"$zero", 0},
-                {"$at", 1},
-                {"$v0", 2},
-                {"$v1", 3},
-                {"$a0", 4},
-                {"$a1", 5},
-                {"$a2", 6},
-                {"$a3", 7},
-                {"$t0", 8},
-                {"$t1", 9},
-                {"$t2", 10},
-                {"$t3", 11},
-                {"$t4", 12},
-                {"$t5", 13},
-                {"$t6", 14},
-                {"$t7", 15},
-                {"$s0", 16},
-                {"$s1", 17},
-                {"$s2", 18},
-                {"$s3", 19},
-                {"$s4", 20},
-                {"$s5", 21},
-                {"$s6", 22},
-                {"$s7", 23},
-                {"$t8", 24},
-                {"$t9", 25},
-                {"$k0", 26},
-                {"$k1", 27},
-                {"$gp", 28},
-                {"$sp", 29},
-                {"$fp", 30},
-                {"$ra", 31},
-            };
+                input[i] = input[i].Trim();
+
+                if (input[i].Contains(".text") || input[i].Length <= 0) { continue; }
+
+                if (openLabel)
+                {
+                    labels[lastOpenLabel] = labels[lastOpenLabel] + 4;
+                    openLabel = false;
+                    continue;
+                }
+
+                Match labelSearch = Regex.Match(input[i], @"^[\w|\d]+:");
+                if (labelSearch.Success)
+                {
+                    string label = labelSearch.Value.Substring(0, labelSearch.Value.Length - 1);
+                    labels[label] = StartAddress + (4 * currentLine);
+                    input[i] = input[i].Substring(labelSearch.Index + labelSearch.Length).Trim();
+                    if (input[i].Length <= 0)
+                    {
+                        lastOpenLabel = label;
+                        openLabel = true;
+                        continue;
+                    }
+                }
+
+                currentLine++;
+            }
         }
     }
 }
--- TP2/Program.cs
+++ TP2/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
-using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace TP2
 {
@@ -8,14 +9,19 @@ namespace TP2
     {
         static void Main(string[] args)
         {
-            if (args.Any() && File.Exists(args[0]))
+            string b = "00001100000100000000000000001000";
+            Console.WriteLine($"0x{Convert.ToInt32(b, 2).ToString("x8")}");
+            return;
+            if (args.Length >= 2 && File.Exists(args[0]))
             {
-                Assembler asm = new Assembler(File.OpenText(args[0]));
+                IEnumerable<string> bin = Assembler.Assemble(File.ReadAllLines(args[0]));
+                File.WriteAllLines(args[1], bin);
 
+                Console.WriteLine("Sucesso!");
                 return;
             }
 
-            Console.WriteLine("Informe o caminho do arquivo.");
+            Console.WriteLine("Informe pela linha de comando os caminhos de entrada e saída, respectivamente.");
         }
     }
 }
